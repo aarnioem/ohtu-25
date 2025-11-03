ae1576f12c1d2651a7244754ea0bb1ae974e016e
aarnioem <aarnioem@gmail.com>
2025-11-03 08:13:18 +0200

    rest of the unit tests

--- viikko1/nhl-statistics/src/tests/statistics_service_test.py
+++ viikko1/nhl-statistics/src/tests/statistics_service_test.py
@@ -12,7 +12,7 @@ class PlayerReaderStub:
             Player("Gretzky", "EDM", 35, 89), # 35+89 = 124
             Player("Turgeon", "BUF", 28, 49), # 34+54 = 88
             Player("Gilmour", "CGY", 26, 59), # 26+59 = 85
-            Player("Coffey", "PIT", 30, 83), # 30+83 = 113
+            Player("Coffey",  "PIT", 30, 83), # 30+83 = 113
         ]
 
 class TestStatisticsService(unittest.TestCase):
@@ -21,7 +21,32 @@ class TestStatisticsService(unittest.TestCase):
             PlayerReaderStub()
         )
     
-    def test_search(self):
+    def test_search_name(self):
         name = "Gilmour"
         
         self.assertEqual(self.stats.search(name), Player("Gilmour", "CGY", 26, 59))
+
+    def test_search_no_results(self):
+        name = "Crosby"
+        
+        self.assertEqual(self.stats.search(name), None)
+
+    def test_team_players(self):
+        edmonton = [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+        self.assertCountEqual(self.stats.team("EDM"), edmonton)
+
+    def test_top_points(self):
+        top_list = [
+            Player("Gretzky", "EDM", 35, 89),
+            Player("Coffey",  "PIT", 30, 83),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Kurri",   "EDM", 37, 53)
+        ]
+
+        self.assertEqual(self.stats.top(4), top_list)
