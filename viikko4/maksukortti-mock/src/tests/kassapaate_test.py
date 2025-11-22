da35ec61fb55a2931ed513736344938a397e04d9
aarnioem <aarnioem@gmail.com>
2025-11-22 13:55:18 +0200

    new tests for kassapaate

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataa_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 2
+
+        self.kassa.lataa(maksukortti_mock, 5)
+
+        maksukortti_mock.lataa.assert_called_with(5)
+
+    def test_lataa_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 2
+
+        self.kassa.lataa(maksukortti_mock, -5)
+
+        maksukortti_mock.lataa.assert_not_called()
